f0d41a4f53aa145388dd341a58e74c4e5df3c184
Michael Vistocci <mikevist68@yahoo.com>
2023-05-11 14:54:21 -0400

    variable exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string dogName = "Layda";
+            int herAge = 2;
+            char herLastInitial = 'V';
+            bool sheIsAGirl = true;
+            double sheIsThisTall = 24.2d;
+            decimal weSpendSoMuch = 100.00m;
+
+
+            Console.WriteLine($" My dog's name is {dogName}. She is {herAge}. Her last initial is {herLastInitial}. " +
+            $"She is a girl {sheIsAGirl}. She is a small dog at {sheIsThisTall}. We spend at least {weSpendSoMuch} a month on her.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
